3dee524f97c3976e77cce018cf306ab3e6213441
shravya-bhaskara <59280388+shravya-bhaskara@users.noreply.github.com>
2026-03-23 22:39:28 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -329,6 +329,7 @@ Seed: ${seed};
                   ...meal,
                   videoUrl: videoUrl || undefined,
                   recipeUrl: recipeUrl || undefined,
+                  recipeFrom
                 };
               }
             }
